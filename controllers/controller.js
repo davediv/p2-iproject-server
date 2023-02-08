2ca52e9e8a580a3ec9fca8b0dde699167850aa43
davediv <33143202+davediv@users.noreply.github.com>
2023-02-08 13:44:38 +0700

    Add Twitter Bio Generator

--- controllers/controller.js
+++ controllers/controller.js
@@ -140,7 +140,7 @@ class Controller {
     } // END OF STATIC
 
 
-    // TWITTER - GENERATE QUOTE
+    // TWITTER - GENERATE QUOTE - DONE
     static async postTwtQuote(req, res) {
 
         try {
@@ -188,6 +188,67 @@ class Controller {
             res.status(200).json({
                 result: resultTrim,
                 log: logs
+            })
+            
+        } catch (err) {
+            console.log(err, '<<<<<<<<< ERR');
+            res.status(500).json({
+                error: 'xxxxx'
+            })
+        }
+
+    } // END OF STATIC
+
+
+    // TWITTER - GENERATE BIO
+    static async postTwtBio(req, res) {
+
+        try {
+            let { name, job, vibe } = req.body
+            console.log(name, job, vibe);
+    
+            // SETUP GPT-3 - text-davinci-003
+            const configuration = new Configuration({
+              apiKey: process.env.OPENAI_API_KEY,
+            });
+            const openai = new OpenAIApi(configuration);
+    
+            // SETUP PROMPT
+            const response = await openai.createCompletion({
+              model: "text-davinci-003",
+              prompt: `Bikin Twitter bio, nama : ${name}, pekerjaan: ${job}, dengan deskripsi yang ${vibe}, dalam bahasa Indonesia, tidak menggunakan hashtag.`,
+              temperature: 0.9,
+              max_tokens: 150,
+              top_p: 1,
+              frequency_penalty: 0,
+              presence_penalty: 0.6,
+              stop: [" Human:", " AI:"],
+            });
+            console.log(response, '<<<<<<<<< response');
+    
+            // TRIM RESULT
+            let resultTrim = response.data.choices[0].text.trim()
+            console.log(resultTrim, '<<<<<<<<< resultTrim');
+
+            // ADD LOG
+            // let postLog = await Log.create({
+            //     // UserId: req.user.id,
+            //     UserId: 1,
+            //     platform: 'Twitter',
+            //     prompt: 'Bio : ' + name + job + vibe,
+            // })
+            // console.log(postLog, '<<<<<<<<< postLog');
+
+
+            // GET LATEST LOGS
+            // let logs = await Log.findAll()
+            // console.log(logs, '<<<<<<<< logs');
+        
+
+            // REST
+            res.status(200).json({
+                result: resultTrim,
+                log: postLog
             })
             
         } catch (err) {
--- routes/route.js
+++ routes/route.js
@@ -15,9 +15,14 @@ router.post('/twitter-tweet', Controller.postTwtTweet);
 // TWITTER - HASHTAG GENERATOR - DONE
 router.post('/twitter-hashtag', Controller.postTwtHashtag);
 
-// TWITTER - QUOTE GENERATOR
+// TWITTER - QUOTE GENERATOR - DONE
 router.post('/twitter-quote', Controller.postTwtQuote)
 
+// TWITTER - BIO GENERATOR
+router.post('/twitter-bio', Controller.postTwtBio)
+
+// TWITTER - FUN FACT GENERATOR
+// router.post('/twitter-fact', Controller.postTwtFact)
 
 
 
